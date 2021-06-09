5163560d3a67d9045c9da128e429aa94d98195a7
Sam Noel <noel.samuel.a@gmail.com>
2021-06-09 09:37:43 -0400

    remove unneeded env variable

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 //if (LOCAL) process.env.NODE_TLS_REJECT_UNAUTHORIZED="0";
 const axios = require('axios');
 const debug = require('debug');
